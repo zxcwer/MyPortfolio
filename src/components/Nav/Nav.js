d4515fd548ba8538e40768206a518f76cc5c4353
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-06-05 22:02:42 +0900

    first commit

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./nav.css";
+const Nav = () => {
+  return (
+    <nav>
+      <a href="#"></a>
+      <a href="#about"></a>
+      <a href="#portfolio"></a>
+      <a href="#contact"></a>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./portfolio.css";
+
+const Portfolio = () => {
+  return <section id="#portfolio" className="section__portfolio"></section>;
+};
+
+export default Portfolio;
