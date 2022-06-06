d81483cca9dd3a10678f7677dfba274805c6b219
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-06-06 15:47:54 +0900

    🌳 update contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
 import { AiOutlineMail, AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+import emailjs from "@emailjs/browser";
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "portfolio",
+        "portfolio_template",
+        form.current,
+        "MXNzIKL3Jy4i39Jxl"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <section id="contact" className="section__contact">
       <h2>Contact Me</h2>
       <div className="container container__contact">
         <div className="contact__items">
           <article className="contact__item">
-            <AiOutlineMail size="2rem" />
+            <AiOutlineMail size="1.5rem" />
             <h3>Email</h3>
-            <h4>keekeoduangdy@gmail.com</h4>
+            <h4>i-keo@tsuyama.kosen-ac.jp</h4>
             <a
-              href="mailto:keekeoduangdy@gmail.com"
+              href="mailto:i-keo@tsuyama.kosen-ac.jp"
               target="_blank"
               className="btn"
             >
@@ -21,7 +44,7 @@ const Contact = () => {
           </article>
 
           <article className="contact__item">
-            <AiFillFacebook size="2rem" />
+            <AiFillFacebook size="1.5rem" />
             <h3>Facebook</h3>
             <h4>Keoduangdy Nitiphon</h4>
             <a
@@ -34,11 +57,11 @@ const Contact = () => {
           </article>
 
           <article className="contact__item">
-            <AiFillInstagram size="2rem" />
+            <AiFillInstagram size="1.5rem" />
             <h3>Instagram</h3>
             <h4>_kk_kdd</h4>
             <a
-              href="mailto:keekeoduangdy@gmail.com"
+              href="https://www.instagram.com/_kk_kdd/"
               target="_blank"
               className="btn"
             >
@@ -46,7 +69,7 @@ const Contact = () => {
             </a>
           </article>
         </div>
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
           <input
             type="text"
             name="name"
@@ -54,21 +77,20 @@ const Contact = () => {
             required
           ></input>
           <input
-            type="text"
+            type="email"
             name="email"
             placeholder="Your Email"
             required
           ></input>
           <textarea
-            type="text"
-            rows="7"
             name="message"
+            rows="7"
             placeholder="Your Message"
             required
           ></textarea>
-          <butto type="summmit" className="btn btn--primary">
+          <button type="submit" className="btn btn--primary">
             Send
-          </butto>
+          </button>
         </form>
       </div>
     </section>
