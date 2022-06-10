60fa7d5c97c9e4bedc8d5e92de40c35bb84b2e2a
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-06-10 11:08:11 +0900

    updated portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
-import me from "../../assets/LittleTurtle.PNG";
+import portfolioData from "./portfolioData";
 const Portfolio = () => {
+  const [data, setData] = React.useState();
   return (
     <section id="portfolio" className="section__portfolio">
       <h2>Portfolio</h2>
@@ -9,18 +10,38 @@ const Portfolio = () => {
         <div className="portfolio__items">
           <article className="portfolio__item">
             <div>
-              <img src={me} className="portfolio__item-image"></img>
+              <img
+                src={portfolioData[0].img}
+                className="portfolio__item-image"
+              ></img>
             </div>
-            <h3>Portfolio</h3>
-            <a
-              href="https://github.com/zxcwer/MyPortfolio"
-              target="_blank"
-              className="btn "
-            >
+            <h3>{portfolioData[0].title}</h3>
+            <a href={portfolioData[0].urlCode} target="_blank" className="btn ">
               Source code
             </a>
             <a
-              href="https://zxcwer.github.io/MyPortfolio/"
+              href={portfolioData[0].urlDemo}
+              target="_blank"
+              className="btn btn--primary"
+            >
+              Demo
+            </a>
+          </article>
+        </div>
+        <div className="portfolio__items">
+          <article className="portfolio__item">
+            <div>
+              <img
+                src={portfolioData[1].img}
+                className="portfolio__item-image"
+              ></img>
+            </div>
+            <h3>{portfolioData[1].title}</h3>
+            <a href={portfolioData[1].urlCode} target="_blank" className="btn ">
+              Source code
+            </a>
+            <a
+              href={portfolioData[1].urlDemo}
               target="_blank"
               className="btn btn--primary"
             >
---
+++ src/components/Portfolio/portfolioData.js
@@ -0,0 +1,18 @@
+import kame from "../../assets/LittleTurtle.PNG";
+import capybara from "../../assets/Capybara.PNG";
+export default [
+  {
+    id: 1,
+    title: "portfolio",
+    img: kame,
+    urlCode: "https://github.com/zxcwer/MyPortfolio",
+    urlDemo: "https://zxcwer.github.io/MyPortfolio/",
+  },
+  {
+    id: 2,
+    title: "NameCard",
+    img: capybara,
+    urlCode: "https://github.com/zxcwer/DigitalNameCard",
+    urlDemo: "https://zxcwer.github.io/DigitalNameCard/",
+  },
+];
